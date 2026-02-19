dc99a4be7d51ef25b5785f4ee37ab8b1b245ec25
PranavNarayan7034 <pranav@expertzlab.com>
2026-02-19 15:43:23 +0530

    home and login updated

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome To Session Based React App</h1>
-            <div>
-                <Link to="/login">Login</Link>
-                <Link to="/register">CreateAccount</Link>
-                <Link to="/profile">Profile</Link>
+        <div className='flex flex-col items-center gap-8 justify-center'>
+            <h1 className='text-3xl font-bold text-center'>Welcome To Session Based React App</h1>
+            <div className='flex gap-4'>
+                <Link className='border-2 border-blue-500/75 px-6 py-2 text-blue-500 rounded-md' to="/login">Login</Link>
+                <Link className='border-2 border-blue-500/75 px-6 py-2 text-blue-500 rounded-md' to="/register">CreateAccount</Link>
+                <Link className='border-2 border-blue-500/75 px-6 py-2 text-blue-500 rounded-md' to="/profile">Profile</Link>
             </div>
         </div>
     )
--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div>
+            <Link className='text-blue-500 ms-4' to="/"> Back To Home</Link>
+            <div className='flex flex-col items-center justify-center h-[calc(100vh-4rem)]'>
+                <div className='bg-amber-200/75 rounded-md p-16'>
+                    <h2 className='text-center text-3xl font-bold mb-12'>Login</h2>
+                    <form className='flex flex-col gap-4'>
+                        <input className='border-2 border-black/50 rounded-lg p-2' type="email" placeholder="Email"/>
+                        <input className='border-2 border-black/50 rounded-lg p-2' type="password" placeholder="Password"/>
+                        <button className='bg-orange-500 text-white rounded-lg p-2' type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
